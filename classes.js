8556ddd2fd6e2f6cf7a5345fc4175b1a6408517b
Valentine-Nkatha <valentinnenkatha@gmail.com>
2024-04-25 19:35:38 +0300

    made changes

--- classes.js
+++ classes.js
@@ -21,12 +21,15 @@ function Rental(car,renterName,rentalStartDate,rentalEndDate){
     rentalEndDate=rentalEndDate
 
     this.calculationRentalDuration=function(){
-        console.log(this.rentalEndDate-this.rentalEndDate)
+        days=rentalEndDate-rentalEndDate
 
     }
 }
 const rentalDays=("Toyota","Total",20,30)
+console.log(rentalDays.rentalStartDate)
+console.log(rentalDays.rentalEndDate)
 console.log(rentalDays.calculationRentalDuration)
+    
 
 
 const newCar = new Car("Subaru","Impreza,2023","true")
@@ -41,23 +44,37 @@ function Question(text,options,corrrectAnswer){
     this.options=options
     this.correctAnswer=this.correctAnswer
 
-    this.checkAnswer=function(){
-
+    this.checkAnswer=function(submittedAnswer){
+        return submittedAnswer===this.correctAnswer
     }
 }
+
 
 function Quiz(questions,currentQuestionIndex,score){
     this.questions=questions
     this.currentQuestionIndex=currentQuestionIndex
     this.score=score
 
-    this.addQuestion=function(){
-
+    this.addQuestion=function(query){
+      this.questions.push(query)
     }
     this.nextQuestion=function(){
+        this.currentQuestionIndex++
 
     }
-    this.submitAnswer=function(){
+    this.submitAnswer=function(submittedAnswer){
+        const questionNow=this.questions[this.currentQuestionIndex];
+        if(questionNow.checkAnswer(submittedAnswer)){
+            this.score++
+        }
+
         
     }
 }
+const quizes=new Quiz()
+const firtstQuestion=new Question("This is good",["This is good","This is bad"])
+quizes.addQuestion(query);
+
+const submittedAnswer="This bad"
+quizes.submitAnswer(submittedAnswer)
+console.log(quizes.score)
